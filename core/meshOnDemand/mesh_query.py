bc4d1dc38799b21ceb5189e56cfdbe8b7f5aa450
Shivendra Gupta <gshivendra234@gmail.com>
2025-10-21 12:20:32 -0400

    Integrated MeSH api

---
+++ core/meshOnDemand/mesh_query.py
@@ -0,0 +1,88 @@
+import requests
+from pathlib import Path
+import json
+from core.meshOnDemand.mesh_utils.mesh_response_parser import extract_from_pre_block
+
+json_query = {
+  "qid": "6e5487fd37ad",
+  "created_at": "2025-10-07T00:32:09Z",
+  "pico_valid": {
+    "Population": "adults with septic shock",
+    "Intervention": "early norepinephrine",
+    "Comparator": "dopamine",
+    "Outcomes": [
+      "28-day mortality"
+    ]
+  },
+  "augmented": {
+    "Population": {
+      "value": "adults with septic shock",
+      "synonyms": [
+        "patients with septic shock",
+        "individuals with septic shock",
+        "patients-with-septic-shock",
+        "individuals-with-septic-shock"
+      ]
+    },
+    "Intervention": {
+      "value": "early norepinephrine",
+      "synonyms": [
+        "norepinephrine",
+        "norepinephrines",
+        "early-norepinephrine"
+      ]
+    },
+    "Comparator": {
+      "value": "dopamine",
+      "synonyms": [
+        "dopamine treatment",
+        "dopamines",
+        "dopamine-treatment"
+      ]
+    },
+    "Outcomes": [
+      {
+        "value": "28-day mortality",
+        "synonyms": [
+          "28-day mortality rate",
+          "28 day mortality rate",
+          "28-day-mortality-rate"
+        ]
+      }
+    ]
+  },
+  "model": "gemma-3 via Vertex",
+  "temperature": 0.2
+}
+
+def query_mesh_api(pico_json=json_query):
+    """
+    Queries the Mesh API with the given Pico query and returns the response.
+
+    Args:
+        pico_json (dict): The Pico query JSON.
+
+    Returns:
+        dict: The txt response from the Mesh API.
+    """
+    headers = {"Content-Type": "application/json"}
+
+    response = requests.post(
+        "https://meshb.nlm.nih.gov/api/MOD",
+        headers=headers,
+        json={"input": json.dumps(pico_json['pico_valid'])},
+    )
+
+
+    if response.status_code == 200:
+        base = Path(__file__).resolve().parent
+        (base.parent / "artifacts_day3" / f"mesh_{pico_json['qid']}.txt").write_text(response.text, encoding="utf-8")
+        mesh_response = extract_from_pre_block(response.text)
+        (base.parent / "artifacts_day3" / f"mesh_parsed{pico_json['qid']}.json").write_text(json.dumps(mesh_response, ensure_ascii=False, indent=2), encoding="utf-8")
+        return response.text
+    else:
+        response.raise_for_status()
+
+if __name__ == "__main__":
+    result = query_mesh_api()
+    print(result)
